b04ef4781c976f26463a55af20e8db1d23f7322d
Hoyoux Adrien <100146992+AdrienHoyoux@users.noreply.github.com>
2024-02-09 16:54:56 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <switchComponent v-model="darkMode" />
+    <div v-if="darkMode" class="dark-mode">
+      <!-- Contenu spécifique au mode sombre -->
+      <h1>Mode Sombre Activé</h1>
+      <!-- Autres éléments du mode sombre -->
+    </div>
+    <div v-else class="light-mode">
+      <!-- Contenu spécifique au mode clair -->
+      <h1>Mode Clair Activé</h1>
+      <!-- Autres éléments du mode clair -->
+    </div>
+  </div>
+</template>
+
+
+<script>
+import switchComponent from './components/switchComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    switchComponent
+  },
+  data() {
+    return {
+      darkMode: true
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  /* Styles communs */
+}
+
+.dark-mode {
+  background-color: #333; /* Couleur de fond pour le mode sombre */
+  color: #fff; /* Couleur du texte pour le mode sombre */
+  height: 98vh;
+}
+
+.light-mode{
+  background-color: aquamarine;
+  color: black;
+  height: 98vh;
+}
+</style>
---
+++ src/components/switchComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-field>
+      <b-switch v-model="localDarkMode" passive-type="is-dark" type="is-warning">
+        {{ localDarkMode ? "Dark Mode" : "Light Mode" }}
+      </b-switch>
+    </b-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'switchComponent',
+  props: {
+    value: Boolean // Utilisez une prop nommée value pour le mode sombre
+  },
+  computed: {
+    localDarkMode: {
+      get() {
+        return !this.value; // Inverser la valeur pour afficher le texte opposé
+      },
+      set(newValue) {
+        this.$emit('input', !newValue); // Inverser la valeur avant d'émettre l'événement
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* Styles spécifiques au composant */
+</style>
